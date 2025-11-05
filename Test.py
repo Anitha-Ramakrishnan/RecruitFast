f11edbd1f4f0ee1c9e6da3d19e303c4cb8cf0888
Anitha-Ramakrishnan <anitha-ramakrishnan@outlook.com>
2025-11-05 10:07:16 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 
 # Initialize Google API key
-API_KEY = "AIzaSyBiwoGW2oc3rhjcN-NTlEGWqoowRCs-dGQ" 
+API_KEY = "xyz" 
 genai.configure(api_key=API_KEY)
 
 # Example skills
